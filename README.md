e04c5de9d4c55be0c95bb6a02f9653fe3959a4cd
Lasse <lassetange@gmail.com>
2022-09-28 13:40:56 +0200

    Adding Intl provider

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## Localizing NextJS app using formatJS and git18n.com
+
+- git18n.com Bot: https://github.com/apps/git18n-com
+- deployment url: https://next-formatjs.vercel.app/
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { IntlProvider } from 'react-intl';
+import en from '../locales/en.json';
+
+const messages: Record<string, {}> = { en };
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const locale = router.query.locale?.toString() || 'en';
+
+  return (
+    <IntlProvider locale={locale} defaultLocale="en" messages={messages[locale]}>
+      <Component {...pageProps} />
+    </IntlProvider>
+  );
 }
 
 export default MyApp;
